321e3af10b03637c8bf5fc699b3887d59428dfb9
ta <akun.rizky.baca@gmail.com>
2021-08-11 16:04:46 +0700

    product model and migration

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/products', function () {
-    return 'products';
+    return Product::all();
+});
+
+Route::post('/products', function () {
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
