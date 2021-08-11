48cc20070dd37774c80b9bd62b7593e67f05a4bb
ta <akun.rizky.baca@gmail.com>
2021-08-12 05:01:27 +0700

    register user and create token

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,15 +19,16 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('products', ProductController::class);
 
 // Public routes
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+  Route::post('/products', [ProductController::class, 'store']);
+  Route::put('/products/{id}', [ProductController::class, 'update']);
+  Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
 
@@ -35,5 +37,5 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
