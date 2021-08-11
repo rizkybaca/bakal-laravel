8d2c695c13c0a86a05b38e632ac4a50bb4161a1d
ta <akun.rizky.baca@gmail.com>
2021-08-11 16:34:12 +0700

    single product and resource routes

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', [ProductController::class, 'index']);
+// Route::get('/products', [ProductController::class, 'index']);
 
-Route::post('/products', [ProductController::class, 'store']);
+// Route::post('/products', [ProductController::class, 'store']);
+
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
