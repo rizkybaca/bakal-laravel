8896127695261b9fafa94a4e5fb934fcac53e52b
ta <akun.rizky.baca@gmail.com>
2021-08-11 16:12:09 +0700

    using the model

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,12 @@ Route::get('/products', function () {
 });
 
 Route::post('/products', function () {
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'product-one',
+        'description' => 'This is product one',
+        'price' => '99.99'
+    ]);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
