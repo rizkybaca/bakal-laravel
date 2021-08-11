0cce487edc59ac01a15530330230e9a260fa3485
ta <akun.rizky.baca@gmail.com>
2021-08-12 04:26:08 +0700

    search product by name

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -68,4 +68,15 @@ class ProductController extends Controller
     {
         return Product::destroy($id);
     }
+
+    /**
+     * Seacrh for a name.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Product::where('name', 'like', '%'.$name.'%')->get();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('products', ProductController::class);
+
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 // Route::get('/products', [ProductController::class, 'index']);
 
 // Route::post('/products', [ProductController::class, 'store']);
 
-Route::resource('products', ProductController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
